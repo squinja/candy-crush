f903233e3916c224984fccf75c057c7cd3923039
Emma <eschaale@u.rochester.edu>
2022-04-26 17:30:42 -0400

    added logic to index js server file to handle user inputted username

--- index.js
+++ index.js
@@ -44,4 +44,112 @@ app.post("/addscore", (req, res) => {
     .catch((err) => res.status(500).json({ message: err }));
 });
 
+app.post("/users", async (req, res) => {
+  try {
+    const username = req.body.username;
+    const response = await axios.get(
+      `${process.env.BASE_URL}users/${username}`,
+      {
+        headers: {
+          Accepts: "application/json",
+          "X-Cassandra-Token": process.env.ASTRA_TOKEN,
+        },
+      }
+    );
+    const data = response.data;
+
+    //
+    if (data.count > 0) {
+      res.status(200).send({
+        msg: "Successfully signed in.",
+        success: true,
+        data: data.data[0],
+      });
+      return;
+    }
+
+    // TODO create the user in the table
+
+    const createResponse = await axios.post(
+      `${process.env.BASE_URL}users`,
+      {
+        userName: username,
+        highscore: 0,
+      },
+      {
+        headers: {
+          Accepts: "application/json",
+          "X-Cassandra-Token": process.env.ASTRA_TOKEN,
+        },
+      }
+    );
+
+    res.status(200).send({
+      msg: "Successfully created username.",
+      success: true,
+      data: createResponse.data,
+    });
+  } catch (error) {
+    console.log(error.response);
+    res.status(500).send("Something went wrong");
+  }
+});
+
+app.patch("/users/:username", async (req, res) => {
+  const highscore = req.body.highscore;
+  const username = req.params.username;
+  try {
+    // UPDATE HIGHSCORE OF USER
+    const response = await axios.patch(
+      `${process.env.BASE_URL}users/${username}`,
+      {
+        highscore,
+      },
+      {
+        headers: {
+          Accepts: "application/json",
+          "X-Cassandra-Token": process.env.ASTRA_TOKEN,
+        },
+      }
+    );
+
+    res.status(200).send("ALL GOOD");
+  } catch (error) {
+    console.log(error.response);
+    res.status(500).send("UH OH");
+  }
+});
+
+app.get("/users", async (req, res) => {
+  const { data } = await axios.get(`${process.env.BASE_URL}users/rows`, {
+    headers: {
+      Accepts: "application/json",
+      "X-Cassandra-Token": process.env.ASTRA_TOKEN,
+    },
+  });
+
+  res.status(200).send({
+    msg: "Retrieved users.",
+    success: true,
+    data,
+  });
+});
+
 app.listen(PORT, () => console.log(`server running on PORT ${PORT}`));
+
+// {
+//     "name": "users",
+//     "columnDefinitions": [
+//       {
+//         "name": "userName",
+//         "typeDefinition: "text"
+//       },
+//       {
+//         "name": "highscore",
+//         "typeDefinition": "integer"
+//       }
+//     ],
+//     "primaryKey": {
+//       "partitionKey": ["userName"]
+//     }
+//   }
