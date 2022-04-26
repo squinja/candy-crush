5290780fd652e1e0e5da0bddbd37ec0d0b5c5c25
Emma <eschaale@u.rochester.edu>
2022-04-26 17:28:48 -0400

    Changed window popup class in winstate component

--- src/components/WinState.js
+++ src/components/WinState.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const WinState = () => {
-  return <h2 className="win-state">You won!</h2>;
+  return <h2 className="window-popup">You won!</h2>;
 };
 
 export default WinState;
