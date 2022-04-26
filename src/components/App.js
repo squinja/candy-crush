b0bbf7d9ca6ad1a9ae8caa0a5a4ecd836c8e9ee1
Emma <eschaale@u.rochester.edu>
2022-04-26 17:34:05 -0400

    Moved user logged in state to app js

--- src/components/App.js
+++ src/components/App.js
@@ -6,15 +6,22 @@ import ScoreBoard from "./ScoreBoard";
 
 const App = () => {
   const [scoreDisplay, setScoreDisplay] = useState(0);
+  const [currentUser, setCurrentUser] = useState(null);
+
   return (
     <div className="app">
       <div className="left-column">
         <Logo />
-        <ScoreBoard score={scoreDisplay} />
+        <ScoreBoard
+          score={scoreDisplay}
+          currentUser={currentUser}
+          setCurrentUser={setCurrentUser}
+        />
       </div>
 
       <GameBoard
         scoreDisplay={scoreDisplay}
+        currentUser={currentUser}
         setScoreDisplay={setScoreDisplay}
       />
     </div>
