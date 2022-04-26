91bd26ce77759e48112a59023e48f5a8db752058
Emma <eschaale@u.rochester.edu>
2022-04-26 17:29:51 -0400

    removed ania kubow's backend implementation and random username logic and implemented user inputted username logic + backend implementation

--- src/components/ScoreBoard.js
+++ src/components/ScoreBoard.js
@@ -1,36 +1,42 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import UsernameInput from "./UsernameInput";
+import HighScoresBoard from "./HighScoresBoard";
 
-const randomUserNames = [
-  "smarmysquid",
-  "runnyrabbit",
-  "weepingwhale",
-  "orangeorangutan",
-  "sleepingsquirrel",
-  "cheekycheetah",
-];
-
-const ScoreBoard = ({ score }) => {
+const ScoreBoard = ({ score, currentUser, setCurrentUser }) => {
   const [userScores, setUserScores] = useState(null);
-  const [userName, setUserName] = useState(null);
+  const [usernameInput, setUsernameInput] = useState("");
+  const [validationError, setValidationError] = useState("");
+
   const fetchData = async () => {
-    const response = await axios.get("http://localhost:8000/scores");
-    const data = Object.keys(response.data.data).map(
-      (item) => response.data.data[item]
-    );
-    setUserScores(data);
+    const { data } = await axios.get("http://localhost:8000/users");
+    console.log("data", data.data.data);
+    setUserScores(data.data.data);
   };
 
-  console.log(userScores);
+  // const saveData = async () => {
+  //   const data = {
+  //     username: userName,
+  //     score: score,
+  //   };
+  //   try {
+  //     await axios.post("http://localhost:8000/addscore", data);
 
-  const saveData = async () => {
-    const data = {
-      username: userName,
-      score: score,
-    };
+  //     await fetchData();
+  //   } catch (error) {
+  //     console.log("error", error);
+  //   }
+  // };
+
+  const saveUserScore = async () => {
+    if (!currentUser) return;
     try {
-      await axios.post("http://localhost:8000/addscore", data);
-
+      const response = await axios.patch(
+        `http://localhost:8000/users/${currentUser}`,
+        {
+          highscore: score,
+        }
+      );
       await fetchData();
     } catch (error) {
       console.log("error", error);
@@ -39,27 +45,51 @@ const ScoreBoard = ({ score }) => {
 
   useEffect(() => {
     fetchData();
-    setUserName(
-      randomUserNames[Math.floor(Math.random() * randomUserNames.length)]
-    );
   }, []);
 
-  const descendingUserScores = userScores?.sort((a, b) => b.score - a.score);
+  const handleUsernameInputChange = (event) => {
+    const { value } = event.target;
+    setUsernameInput(value);
+  };
+
+  const handleUsernameSubmit = async (event) => {
+    event.preventDefault();
+    if (!usernameInput) return;
+
+    // Send username to backend
+    try {
+      const { data } = await axios.post("http://localhost:8000/users", {
+        username: usernameInput,
+      });
+      console.log("data", data);
+      setValidationError(data.msg);
+      setCurrentUser(data.data.userName);
+      await fetchData();
+    } catch (error) {
+      console.log("error", error);
+    }
+    setUsernameInput("");
+  };
 
   return (
-    <div className="score-board">
-      <h2>Score: {score}</h2>
-      <h3>Username: {userName}</h3>
-      <h3>Get to 35 to win!</h3>
-      <h3>High Scores:</h3>
-      {descendingUserScores?.map((userScore, index) => (
-        <div key={{ index }}>
-          <h3>
-            {userScore.username}: {userScore.score}
-          </h3>
-        </div>
-      ))}
-      <button onClick={saveData}>save score</button>
+    <div>
+      <div className="score-board">
+        <h2>{!currentUser ? "Log in to play" : `Score: ${score}`}</h2>
+        {validationError && <p>{validationError}</p>}
+        <form onSubmit={handleUsernameSubmit}>
+          <input
+            type="text"
+            placeholder="Username"
+            value={usernameInput}
+            onChange={handleUsernameInputChange}
+          />
+          <button>Submit</button>
+        </form>
+        <h3>Username: {currentUser || "Not signed in"}</h3>
+        <h3>Get to 35 to win!</h3>
+        <button onClick={saveUserScore}>Save your score</button>
+      </div>
+      <HighScoresBoard userScores={userScores} />
     </div>
   );
 };
