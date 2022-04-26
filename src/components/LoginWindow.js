251c6e458d6bb53fa247fee35a6a529f826b0ac1
Emma <eschaale@u.rochester.edu>
2022-04-26 17:30:11 -0400

    Login window to display over game board before user logs in

---
+++ src/components/LoginWindow.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LoginWindow = () => {
+  return (
+    <div className="window-popup">
+      <h2>Log in to play</h2>
+    </div>
+  );
+};
+
+export default LoginWindow;
