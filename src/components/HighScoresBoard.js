e775c05f8db9cc2943d4ca306b8d40a019509d40
Emma <eschaale@u.rochester.edu>
2022-05-03 15:34:52 -0400

    Added handling for when backend hasn't loaded data yet to not display user scores

--- src/components/HighScoresBoard.js
+++ src/components/HighScoresBoard.js
@@ -1,20 +1,32 @@
 import React from "react";
 
-const HighScoresBoard = ({ userScores }) => {
-  const descendingUserScores = userScores?.sort(
+const HighScoresBoard = ({
+  userScores,
+  loadedData,
+  deleteUser,
+  currentUser,
+}) => {
+  const descendingUserScores = userScores.sort(
     (a, b) => b.highscore - a.highscore
   );
 
   return (
     <div className="high-scores-board">
       <h2>High Scores:</h2>
-      {descendingUserScores?.map((userScore, index) => (
-        <div className="align-left" key={index}>
-          <h3>
-            {index + 1}. {userScore.userName}: {userScore.highscore}
-          </h3>
-        </div>
-      ))}
+      {loadedData ? (
+        descendingUserScores.map((userScore, index) => (
+          <div className="align-left" key={index}>
+            <h3>
+              {index + 1}. {userScore.userName}: {userScore.highscore}
+            </h3>
+            {currentUser === userScore.userName ? (
+              <button onClick={() => deleteUser(userScore.userName)}>X</button>
+            ) : null}
+          </div>
+        ))
+      ) : (
+        <h2>Loading...</h2>
+      )}
     </div>
   );
 };
