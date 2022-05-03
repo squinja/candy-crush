81650f3929283291a3d73a397f097d3120bf074e
Emma <eschaale@u.rochester.edu>
2022-05-03 15:32:39 -0400

    Added ability to delete user, error for validating user if username includes non a-z chars, and useeffect to automatically save user score when won

--- src/components/ScoreBoard.js
+++ src/components/ScoreBoard.js
@@ -3,40 +3,30 @@ import { useEffect, useState } from "react";
 import HighScoresBoard from "./HighScoresBoard";
 
 const ScoreBoard = ({ score, currentUser, setCurrentUser }) => {
-  const [userScores, setUserScores] = useState(null);
+  const [userScores, setUserScores] = useState([]);
   const [usernameInput, setUsernameInput] = useState("");
   const [validationError, setValidationError] = useState("");
+  const [loadedData, setLoadedData] = useState(false);
 
+  // This function handles updating the user scores from the backend on useEffect
   const fetchData = async () => {
     const { data } = await axios.get(
+      // Below ternary operator handles for both production and development server
       `${
         process.env.NODE_ENV === "development"
           ? "http://localhost:8000"
           : "https://protected-oasis-38267.herokuapp.com"
       }/users`
     );
-    console.log("data", data.data.data);
     setUserScores(data.data.data);
+    setLoadedData(true);
   };
 
-  // const saveData = async () => {
-  //   const data = {
-  //     username: userName,
-  //     score: score,
-  //   };
-  //   try {
-  //     await axios.post("http://localhost:8000/addscore", data);
-
-  //     await fetchData();
-  //   } catch (error) {
-  //     console.log("error", error);
-  //   }
-  // };
-
   const saveUserScore = async () => {
+    // If user is not logged in, cancel out of this function
     if (!currentUser) return;
     try {
-      const response = await axios.patch(
+      await axios.patch(
         `${
           process.env.NODE_ENV === "development"
             ? "http://localhost:8000"
@@ -46,6 +36,7 @@ const ScoreBoard = ({ score, currentUser, setCurrentUser }) => {
           highscore: score,
         }
       );
+      // Update the userscore to the high score board when the user saves their score for immediate feedback
       await fetchData();
     } catch (error) {
       console.log("error", error);
@@ -55,6 +46,19 @@ const ScoreBoard = ({ score, currentUser, setCurrentUser }) => {
   useEffect(() => {
     fetchData();
   }, []);
+
+  useEffect(() => {
+    let timer;
+    if (score > 34) {
+      timer = setTimeout(async () => {
+        await saveUserScore();
+      }, 1000);
+    }
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [score]);
 
   const handleUsernameInputChange = (event) => {
     const { value } = event.target;
@@ -77,14 +81,36 @@ const ScoreBoard = ({ score, currentUser, setCurrentUser }) => {
           username: usernameInput,
         }
       );
-      console.log("data", data);
+
       setValidationError(data.msg);
       setCurrentUser(data.data.userName);
+
       await fetchData();
     } catch (error) {
-      console.log("error", error);
+      setValidationError(error.response.data);
     }
     setUsernameInput("");
+  };
+
+  // To delete users on high score board
+  const deleteUser = async (username) => {
+    // send delete request to backend
+    try {
+      await axios.delete(
+        `${
+          process.env.NODE_ENV === "development"
+            ? "http://localhost:8000"
+            : "https://protected-oasis-38267.herokuapp.com"
+        }/users/${username}`
+      );
+      // await fetchData();
+      const scoresAfterDeletion = userScores.filter(
+        (user) => user.userName !== username
+      );
+      setUserScores(scoresAfterDeletion);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
@@ -92,20 +118,27 @@ const ScoreBoard = ({ score, currentUser, setCurrentUser }) => {
       <div className="score-board">
         <h2>{!currentUser ? "Log in to play" : `Score: ${score}`}</h2>
         {validationError && <p>{validationError}</p>}
-        <form onSubmit={handleUsernameSubmit}>
-          <input
-            type="text"
-            placeholder="Username"
-            value={usernameInput}
-            onChange={handleUsernameInputChange}
-          />
-          <button>Submit</button>
-        </form>
+        {!currentUser ? (
+          <form onSubmit={handleUsernameSubmit}>
+            <input
+              type="text"
+              placeholder="Username"
+              value={usernameInput}
+              onChange={handleUsernameInputChange}
+            />
+            <button>Submit</button>
+          </form>
+        ) : null}
         <h3>Username: {currentUser || "Not signed in"}</h3>
         <h3>Get to 35 to win!</h3>
         <button onClick={saveUserScore}>Save your score</button>
       </div>
-      <HighScoresBoard userScores={userScores} />
+      <HighScoresBoard
+        userScores={userScores}
+        loadedData={loadedData}
+        deleteUser={deleteUser}
+        currentUser={currentUser}
+      />
     </div>
   );
 };
